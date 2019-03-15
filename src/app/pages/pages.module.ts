4f5d253db2f1f6d8bb1d36e33ccc1d56bb8d8fd9
aflores1010 <p4ul1991@gmail.com>
2019-03-14 22:55:20 -0500

    first commit

---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,23 @@
+import { HomeComponent } from './home/home.component';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+
+@NgModule({
+  declarations: [
+    HomeComponent,
+    AboutComponent,
+    ContactComponent
+  ],
+  imports: [
+    CommonModule,
+
+  ],
+  exports: [
+    HomeComponent,
+    AboutComponent,
+    ContactComponent
+  ]
+})
+export class PagesModule { }
